45396856ccfe9d9d7e93dc97e244418308daf0a2
Soumyajit Das <mepioneiro@gmail.com>
2022-08-18 20:27:18 +0530

    cart api update, delete

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,6 +1,6 @@
 import db from "../../lib/mongodb";
 
-const methodsAllowed = ["GET", "POST"];
+const methodsAllowed = ["GET", "POST", "PUT", "DELETE"];
 
 const handler = async (req, res) => {
   if (!methodsAllowed.includes(req.method))
@@ -17,29 +17,57 @@ const handler = async (req, res) => {
   if (result === null)
     return res.status(404).json({ message: "User Not Found" });
 
-  const { cart = [] } = result;
+  let { cart = [] } = result;
 
   switch (req.method) {
     case "GET":
-      const count = cart.reduce((previous, current) => {
-        return previous + current.quantity;
-      }, 0);
+      {
+        const count = cart.reduce((previous, current) => {
+          return previous + current.quantity;
+        }, 0);
 
-      res.json({ cart, count });
-
+        res.json({ cart, count });
+      }
       break;
     case "POST":
-      const { productid, quantity = 1 } = req.body;
+      {
+        const { productid, quantity = 1 } = req.body;
 
-      let product = cart.find((e) => e.productid === productid);
+        let product = cart.find((e) => e.productid === productid);
 
-      if (!product) cart.push((product = { productid, quantity: 0 }));
-      product.quantity += quantity;
+        if (!product) cart.push((product = { productid, quantity: 0 }));
+        product.quantity += quantity;
 
-      await users.findOneAndUpdate({ email }, { $set: { ...result, cart } });
+        await users.findOneAndUpdate({ email }, { $set: { ...result, cart } });
 
-      res.status(200).json({ message: "success" });
+        res.status(200).json({ message: "success" });
+      }
+      break;
+    case "PUT":
+      {
+        const { productid, quantity } = req.body;
 
+        const product = cart.find((e) => e.productid === productid);
+
+        product.quantity = quantity;
+
+        cart = cart.filter((e) => e.quantity > 0);
+
+        await users.findOneAndUpdate({ email }, { $set: { ...result, cart } });
+
+        res.status(200).json({ message: "success" });
+      }
+      break;
+    case "DELETE":
+      {
+        const { productid } = req.body;
+
+        cart = cart.filter((e) => e.productid !== productid);
+
+        await users.findOneAndUpdate({ email }, { $set: { ...result, cart } });
+
+        res.status(200).json({ message: "success" });
+      }
       break;
   }
 };
